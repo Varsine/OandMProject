f85c968c95899fdbdd0b6b2838edf69f4ca3373e
Varsine <varsulmozg@gmail.com>
2021-08-29 17:15:14 +0400

    logo svg animation

--- src/containers/Home/Main/index.jsx
+++ src/containers/Home/Main/index.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { activeIndexSelector, modeSelector } from 'slices/mainSlice';
 
 import styles from './Main.scss';
 
-import { LogoLargeIcon } from '../../../icons';
+import { LogoAnimaIcon, LogoMoveBallIcon } from '../../../icons';
 
 const Main = () => {
+  const activeIndex = useSelector(activeIndexSelector);
+  const isDarkMode = useSelector(modeSelector);
+  const isActive = activeIndex === 1;
+  const [isAnimate, setIsAnimate] = useState(false);
+
+  useEffect(() => {
+    if (isActive) {
+      setIsAnimate(true);
+    } else {
+      setIsAnimate(false);
+    }
+  }, [activeIndex]);
+
   return (
     <section id="header" className="section">
       <div className="canvas__working" />
       <div className={styles.wrapper}>
-        <LogoLargeIcon />
+        {isAnimate && <LogoMoveBallIcon className={styles.wrapper__ball} />}
+        {isAnimate && (
+          <LogoAnimaIcon
+            className={styles.wrapper__logo}
+            stopColor={isDarkMode ? '#fff' : '#333'}
+          />
+        )}
+        {!isAnimate && <LogoAnimaIcon />}
       </div>
     </section>
   );
---
+++ src/icons/LogoMoveBallIcon.jsx
@@ -0,0 +1,26 @@
+const LogoMoveBallIcon = ({ ...rest }) => (
+  <svg
+    width="1920"
+    height="1080"
+    viewBox="0 11 1920 1080"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...rest}
+  >
+    <rect width="77" height="77" rx="38.5" fill="url(#paint0_linear)" />
+    <defs>
+      <linearGradient
+        id="paint0_linear"
+        x1="38.5"
+        y1="0"
+        x2="38.5"
+        y2="77"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5040B6" />
+        <stop offset="1" stopColor="#3D80D4" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default LogoMoveBallIcon;
