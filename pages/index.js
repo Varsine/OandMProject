bdf2cf82c65df4cedea69bbbde6a57e504b87d57
Hen-arm <armenakyan96@gmail.com>
2021-10-08 13:12:44 +0400

    add seo optimization

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,18 @@ import { HelmetLayout } from 'layouts/index';
 import { HomeContainer } from 'containers/index';
 
 const HomePage = () => (
-  <HelmetLayout title="Home" metaDescription="Home page">
+  <HelmetLayout
+    title="ShellLogix LLC"
+    metaDescription="
+  ShellLogix LLC has been established in 2017 with a mission to 
+  offer creative solutions in the field of We believe that contemporary ideas and technologies are the key
+          milestones for creating strategic solutions for traditions based
+          businesses.Our team members are fully engaged in the process of
+          content making and have got unique affection for their job. One of
+          the main advantages of our company is the productivity based working
+          structure which is our straight response to the time and spendings
+          of our clients as we highly estimate your choice and trust in us"
+  >
     <HomeContainer />
   </HelmetLayout>
 );
