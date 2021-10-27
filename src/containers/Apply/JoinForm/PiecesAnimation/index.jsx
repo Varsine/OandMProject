32f30ed3fa0ed427571a05a20af74b5515e3f816
Martirosyan Mrd <martirosyanmrd@gmail.com>
2021-10-27 21:19:54 +0400

    finished apllay now animation

--- src/containers/Apply/JoinForm/PiecesAnimation/index.jsx
+++ src/containers/Apply/JoinForm/PiecesAnimation/index.jsx
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import { calculatePiecesPosition } from 'utils/index';
+import { noop, calculatePiecesPosition } from 'utils/index';
 
 import styles from './PiecesAnimation.scss';
 
-const PiecesAnimation = () => {
-  const [isAnimate, setIsAnimate] = useState(false);
-
+const PiecesAnimation = ({ isAnimate, animClickHanlder }) => {
   const piecesList = Array.from({ length: 168 }, (_, i) => ({
     id: i + 1,
   }));
 
-  const renderPiecesList = piecesList
-    // ${styles.cut_images_containerTL__cubs}
-    .map(({ id }, index) => (
-      <div
-        key={id}
-        role="button"
-        aria-label="img"
-        className={`${styles.cut_images_item}`}
-        style={{
-          backgroundPosition: calculatePiecesPosition(index),
-        }}
-      />
-    ));
+  const renderPiecesList = piecesList.map(({ id }, index) => (
+    <div
+      key={id}
+      role="button"
+      aria-label="img"
+      className={`${styles.cut_images_item}`}
+      style={{
+        backgroundPosition: calculatePiecesPosition(index),
+      }}
+    />
+  ));
 
   return (
     <div
       role="button"
-      onClick={() => setIsAnimate(!isAnimate)}
+      onClick={animClickHanlder}
       className={classNames(styles.cut_images, {
         [styles.cut_images_active]: isAnimate,
       })}
@@ -39,6 +36,14 @@ const PiecesAnimation = () => {
   );
 };
 
-PiecesAnimation.propTypes = {};
+PiecesAnimation.propTypes = {
+  isAnimate: PropTypes.bool,
+  animClickHanlder: PropTypes.func,
+};
+
+PiecesAnimation.defaultProps = {
+  isAnimate: false,
+  animClickHanlder: noop,
+};
 
 export default PiecesAnimation;
--- src/containers/Apply/JoinForm/index.jsx
+++ src/containers/Apply/JoinForm/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 import axios from 'axios';
 
 import { Button } from 'components/index';
+import { modeSelector } from 'slices/mainSlice';
 
 import Success from './Steps/Success';
 import StepOne from './Steps/StepOne';
@@ -13,16 +15,14 @@ import styles from './Apply.scss';
 import { StepIcon } from '../../../icons';
 
 const JoinForm = () => {
+  const isDarkMode = useSelector(modeSelector);
+
   const [applicationForm, setApplicationForm] = useState({});
   const [activeIndex, setIsActiveIndex] = useState(1);
   const [renderAnimation, setRenderAnimation] = useState(false);
 
   const editActiveStep = (step) => {
     setIsActiveIndex(step);
-  };
-
-  const animationHAndler = () => {
-    setRenderAnimation(false);
   };
 
   const sendApplicationHandler = async (values) => {
@@ -37,64 +37,63 @@ const JoinForm = () => {
     );
   };
 
+  const animClickHanlder = () => {
+    setRenderAnimation(true);
+  };
+
   return (
     <section className="section">
-      <div
-        type="submit"
-        role="button"
-        onClick={animationHAndler}
-        className={styles.container}
-      >
-        {!renderAnimation ? (
-          <PiecesAnimation />
-        ) : (
-          <div
-            style={{ marginTop: activeIndex === 3 ? 100 : 0 }}
-            className={styles.wrapper}
-          >
-            <h1 className={styles.title}>Apply Now!</h1>
-            {activeIndex !== 3 && (
-              <div>
-                <p className={styles.subtitle}>Submit your information here!</p>
-                <div className={styles.steps}>
-                  <Button
-                    onClick={() => editActiveStep(1)}
-                    className={classNames(styles.steps__item, {
-                      [styles.steps__item_active]: activeIndex >= 1,
-                    })}
-                  >
-                    1
-                  </Button>
-                  <StepIcon />
-                  <Button
-                    disabled={activeIndex === 1}
-                    onClick={() => editActiveStep(2)}
-                    className={classNames(styles.steps__item, {
-                      [styles.steps__item_active]: activeIndex === 2,
-                    })}
-                  >
-                    2
-                  </Button>
-                </div>
+      <div className={styles.container}>
+        <PiecesAnimation
+          isAnimate={renderAnimation}
+          animClickHanlder={animClickHanlder}
+        />
+        <div
+          style={{ marginTop: activeIndex === 3 ? 100 : 0 }}
+          className={!renderAnimation ? styles.wrapper : styles.wrapper_anima}
+        >
+          <h1 className={styles.title}>Apply Now!</h1>
+          {activeIndex !== 3 && (
+            <div>
+              <p className={styles.subtitle}>Submit your information here!</p>
+              <div className={styles.steps}>
+                <Button
+                  onClick={() => editActiveStep(1)}
+                  className={classNames(styles.steps__item, {
+                    [styles.steps__item_active]: activeIndex >= 1,
+                  })}
+                >
+                  1
+                </Button>
+                <StepIcon className={isDarkMode || styles.steps__line} />
+                <Button
+                  disabled={activeIndex === 1}
+                  onClick={() => editActiveStep(2)}
+                  className={classNames(styles.steps__item, {
+                    [styles.steps__item_active]: activeIndex === 2,
+                  })}
+                >
+                  2
+                </Button>
               </div>
-            )}
-            {activeIndex === 1 && (
-              <StepOne
-                setApplicationForm={setApplicationForm}
-                editActiveStep={editActiveStep}
-              />
-            )}
-            {activeIndex === 2 && (
-              <StepTwo
-                sendApplicationHandler={sendApplicationHandler}
-                applicationForm={applicationForm}
-                setApplicationForm={setApplicationForm}
-                editActiveStep={editActiveStep}
-              />
-            )}
-            {activeIndex === 3 && <Success />}
-          </div>
-        )}
+            </div>
+          )}
+          {activeIndex === 1 && (
+            <StepOne
+              setApplicationForm={setApplicationForm}
+              editActiveStep={editActiveStep}
+            />
+          )}
+          {activeIndex === 2 && (
+            <StepTwo
+              sendApplicationHandler={sendApplicationHandler}
+              applicationForm={applicationForm}
+              setApplicationForm={setApplicationForm}
+              editActiveStep={editActiveStep}
+            />
+          )}
+          {activeIndex === 3 && <Success />}
+        </div>
       </div>
     </section>
   );
