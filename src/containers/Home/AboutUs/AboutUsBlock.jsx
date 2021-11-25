0a80c00941997a9c233124057d1215eb0e6f0cf0
Varsine <varsulmozg@gmail.com>
2021-11-25 11:19:23 +0400

    fix conflict

--- src/containers/Home/AboutUs/AboutUsBlock.jsx
+++ src/containers/Home/AboutUs/AboutUsBlock.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { QuestionOctagon } from '@styled-icons/bootstrap/QuestionOctagon';
 
 import { NextImage } from 'components/index';
 
@@ -9,8 +10,15 @@ const AboutUsBlock = ({ src, className, classRope, handlerChange }) => {
   return (
     <div className={`${styles.block} ${className}`}>
       <div className={`${styles.block__rope} ${classRope}`} />
-      <div role="button" onClick={handlerChange}>
-        <NextImage src={src} className={styles.block__image} loading="eager" />
+      <div className={styles.block__img} role="button" onClick={handlerChange}>
+        <div className={styles.block__img_wrapper}>
+          <QuestionOctagon className={styles.block__img_wrapper_icon} />
+        </div>
+        <NextImage
+          src={src}
+          className={styles.block__img__image}
+          loading="eager"
+        />
       </div>
     </div>
   );
