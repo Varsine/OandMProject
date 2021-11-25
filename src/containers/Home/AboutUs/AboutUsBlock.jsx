597e8421d2cbe702383c303c86adde099f8473a2
Varsine <varsulmozg@gmail.com>
2021-11-25 17:33:29 +0400

    remove unnecessary code

--- src/containers/Home/AboutUs/AboutUsBlock.jsx
+++ src/containers/Home/AboutUs/AboutUsBlock.jsx
@@ -6,23 +6,21 @@ import { NextImage } from 'components/index';
 
 import styles from './AboutUs.scss';
 
-const AboutUsBlock = ({ src, className, classRope, handlerChange }) => {
-  return (
-    <div className={`${styles.block} ${className}`}>
-      <div className={`${styles.block__rope} ${classRope}`} />
-      <div className={styles.block__img} role="button" onClick={handlerChange}>
-        <div className={styles.block__img_wrapper}>
-          <QuestionOctagon className={styles.block__img_wrapper_icon} />
-        </div>
-        <NextImage
-          src={src}
-          className={styles.block__img__image}
-          loading="eager"
-        />
+const AboutUsBlock = ({ src, className, classRope, handlerChange }) => (
+  <div className={`${styles.block} ${className}`}>
+    <div className={`${styles.block__rope} ${classRope}`} />
+    <div className={styles.block__img} role="button" onClick={handlerChange}>
+      <div className={styles.block__img_wrapper}>
+        <QuestionOctagon className={styles.block__img_wrapper_icon} />
       </div>
+      <NextImage
+        src={src}
+        className={styles.block__img__image}
+        loading="eager"
+      />
     </div>
-  );
-};
+  </div>
+);
 
 AboutUsBlock.propTypes = {
   src: PropTypes.string,
